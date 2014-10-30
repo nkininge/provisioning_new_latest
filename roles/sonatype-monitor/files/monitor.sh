32396d8698299640ea3565aadd49d37be4c3b644
Jeff Bowles <jbowles@hortonworks.com>
2014-10-30 00:33:00 -0700

    Making the nexus-monitor take the parameter "upperbound" from command line.
    If you say, "upper bound is 80" then at 80% you'll start getting warnings.

--- roles/sonatype-monitor/files/monitor.sh
+++ roles/sonatype-monitor/files/monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script that will send an email to EMAIL when disk use in partition PART is bigger than %MAX
 # adapt these 3 parameters to your case
-MAX=50
+MAX=${MAX:="80"}
 #EMAIL=gkesavan@hortonworks.com
 # EMAIL=releng-group@hortonworks.com
 PART=/dev/sdf
@@ -26,7 +26,7 @@ fi
 USE=`df $PART0 | grep -v Filesystem|  awk '{ print $5 }' | cut -d'%' -f1`
 if [ $USE -gt $MAX ]; then
   echo "REF https://hortonworks.jira.com/browse/BUG-12744?focusedCommentId=74279 \
-for cleanup info" && echo "Nexus Internal  (root filesystem)Running out of disk space / : $USE Percent used"  && echo ""
+for cleanup info" && echo "Nexus Internal  (root filesystem) Running out of disk space / : $USE Percent used"  && echo ""
    status=1
 fi
 
