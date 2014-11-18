26ec46a28ea1c8175df6dd8c6b14e6fdbec28193
Jeff Bowles <jbowles@hortonworks.com>
2014-11-17 19:35:09 -0800

    Adding script to monitor Jenkins slave disk usage.

---
+++ roles/jenkins-slave-monitor/files/monitor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# script that will send an email to EMAIL when disk use in partition PART is bigger than %MAX
+# adapt these 3 parameters to your case
+MAX=${MAX:="90"}
+PART=/
+PART1=/grid/0/jenkins/workspace
+ 
+status=0
+USE=`df $PART | grep -v Filesystem|  awk '{ print $5 }' | cut -d'%' -f1`
+if [ $USE -gt $MAX ]; then
+  echo "Jenkins ROOT FILESYSTEM Running out of disk space /grid/0 : $USE Percent used"  && echo ""
+  status=1
+fi
+
+USE=`df $PART1 | grep -v Filesystem|  awk '{ print $5 }' | cut -d'%' -f1`
+if [ $USE -gt $MAX ]; then
+  echo "Jenkins disk space in $PART1 Running out of space : $USE Percent used" && echo ""
+  status=1
+fi
+
+exit $status
+
