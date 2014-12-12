ce75ccbb4a268622e175ee7569892c1766e29e9f
Aki Sasaki <asasaki@hortonworks.com>
2014-12-12 15:28:43 -0800

    BUG-29082 - monitor jenkins slaves for inodes as well

--- roles/jenkins-slave-monitor/files/monitor.sh
+++ roles/jenkins-slave-monitor/files/monitor.sh
@@ -4,19 +4,23 @@
 MAX=${MAX:="90"}
 PART=/
 PART1=/grid/0/jenkins/workspace
- 
+
+function check_usage {
+    if [ $1 -gt $MAX ] ; then
+        echo "Jenkins $2 running out of $3: $1 percent used" && echo ""
+    fi
+    status=1
+}
+
 status=0
 USE=`df $PART | grep -v Filesystem|  awk '{ print $5 }' | cut -d'%' -f1`
-if [ $USE -gt $MAX ]; then
-  echo "Jenkins ROOT FILESYSTEM Running out of disk space /grid/0 : $USE Percent used"  && echo ""
-  status=1
-fi
+check_usage $USE "ROOT FILESYSTEM" "disk space"
+USE=`df -i $PART | grep -v Filesystem|  awk '{ print $5 }' | cut -d'%' -f1`
+check_usage $USE "ROOT FILESYSTEM" "inodes"
 
 USE=`df $PART1 | grep -v Filesystem|  awk '{ print $5 }' | cut -d'%' -f1`
-if [ $USE -gt $MAX ]; then
-  echo "Jenkins disk space in $PART1 Running out of space : $USE Percent used" && echo ""
-  status=1
-fi
+check_usage $USE $PART1 "disk space"
+USE=`df -i $PART1 | grep -v Filesystem|  awk '{ print $5 }' | cut -d'%' -f1`
+check_usage $USE $PART1 "inodes"
 
 exit $status
-
