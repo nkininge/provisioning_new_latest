b47bf8f36a0c907f3dcd2d151e68e2464a1c8832
Shreelola Hegde <shrilola.r@gmail.com>
2019-06-26 15:44:51 +0530

    RELENG-5968: Add build infra slave base image
    Change-Id: I09322e321c9b0e4a2c8afe875cfd60e032a25839

--- roles/common/files/hw-caching-proxy.sh
+++ roles/common/files/hw-caching-proxy.sh
@@ -1,2 +1,2 @@
-export http_proxy='http://hwx-eng:5XD6xKQDiKDjOD@cache.eng.hortonworks.com:1280'
+#export http_proxy='http://hwx-eng:5XD6xKQDiKDjOD@cache.eng.hortonworks.com:1280'
 export no_proxy='0.0.0.0,localhost,127.0.0.1,.openstacklocal,.novalocal,169.254.169.254,openstack.eng.hortonworks.com,dashboard.qe.hortonworks.com,172.22.192.250,192.175.27.100,ad-ec2.qe.hortonworks.com,ad-nano.qe.hortonworks.com'
